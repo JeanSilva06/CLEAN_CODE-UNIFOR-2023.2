c2527663bda3cebe82693b950dd5dcc61a5e1ce8
JeanSilva06 <jeansilva@edu.unifor.br>
2024-10-16 20:29:33 -0300

    Update 01-sample.js

--- 01 - Significative names/01-sample.js
+++ 01 - Significative names/01-sample.js
@@ -1,3 +1,4 @@
+//antigo
 function h(a) {
     let b = [];
     for (let i = 0; i < a.length; i++) {
@@ -11,3 +12,27 @@ function h(a) {
 let x = [1, 2, 3, 4, 5, 6];
 let y = h(x);
 console.log(y);
+
+//novo
+function filtrarNumerosPares(sequenciaNumericaDeAlunos) { //parametro era chamado array, talvez gerasse margem para interpretações multiplas aí ficou melhor identificar para quem seria o método
+    let numerosParesEncontrados = []; 
+    for (let indice = 0; indice < sequenciaNumericaDeAlunos.length; indice++) {
+        if (sequenciaNumericaDeAlunos[indice] % 2 === 0) {
+            numerosParesEncontrados.push(sequenciaNumericaDeAlunos[indice]);
+        }
+    }
+    return numerosParesEncontrados;
+}
+
+let sequenciaNumericaDeAlunos = [1, 2, 3, 4, 5, 6]; //antigo nome era listaDeNumeros --> não era tão descritivo pois não tinha finalidade
+let numerosPares = filtrarNumerosPares(sequenciaNumericaDeAlunos);
+console.log(numerosPares);
+
+
+// class Carro {
+//         modelo = '';
+//         constructor(modelo, ano, marca) {
+//             this.modelo = modelo;
+//         }
+
+// }
